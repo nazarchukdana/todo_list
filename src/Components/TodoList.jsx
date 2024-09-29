12b3995794eeca2cf4bf52d791b1f019b4a069a1
nazarchukdana <nazarchuk.dana05@gmail.com>
2024-09-29 15:29:47 -0400

    adding an event OnClick for delete button

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -48,7 +48,7 @@ const TodoList = () => {
             <div key={index} className="todo-card">
                 <div className="heading_todo">
                     <h3>{todo.heading}</h3> {/* Display the heading here */}
-                    <button className="delete-button-heading">Delete Heading </button>
+                    <button className="delete-button-heading" onClick={handleDeleteTodo(index)}>Delete Heading </button>
                 </div>
                 <div className="add_list">
                     <ul>
