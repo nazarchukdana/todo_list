59272b8133b070dd5e60bdb8b5aad18b5b4242e0
nazarchukdana <nazarchuk.dana05@gmail.com>
2024-09-29 14:38:55 -0400

    adding variables todos, headingInput, listInputs and setting to default values, adding a function handleAddTodo to handle adding of todos, adding value to heading input and onChange event, adding to a button onCick event

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+    const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+  const handleAddTodo = () => {
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
 
   return (
     <>
@@ -13,9 +21,10 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
-          />
-          <button className="add-list-button">Add Heading</button>
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}} 
+        />
+            <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
